4f87c6ffd39840e606ad10aabf4cc050cdfdc960
2º DAW - Tarde <bentexui@yahoo.es>
2014-11-27 18:11:15 +0000

    B1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       //proyecto con ramas
+        
+        if(isset($_POST["enviar"])){
+        
+        $num1=$_POST["num1"];
+        $num=$_POST["num2"];
+        }
+        ?>
+    </body>
+</html>
