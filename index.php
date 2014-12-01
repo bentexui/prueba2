5fd7a4daa85457a74926bf67781f70b36451f8d6
2º DAW - Tarde <bentexui@yahoo.es>
2014-12-01 17:20:36 +0000

    G2

--- index.php
+++ index.php
@@ -12,12 +12,18 @@ and open the template in the editor.
     <body>
         <?php
        //proyecto con ramas
+        $operacion = 0;
+        $resultado=0;
+        $op1=0;
+        $op2=0;
         
-        if(isset($_POST["enviar"])){
         
-        $num1=$_POST["num1"];
-        $num=$_POST["num2"];
-        }
+        if (isset($_GET['calcular'])){
+    $op1 = $_GET["op1"];
+    $op2 = $_GET["op2"];
+
+}
+        
         ?>
     </body>
 </html>
